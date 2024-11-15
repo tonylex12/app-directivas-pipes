db0bec103df6387f130ac38d2e176d0d16aeaac4
Tony HUerto <tonylex12@gmail.com>
2024-11-15 13:49:37 -0500

    ejemplos de pipes

--- src/app/ejemplo-pipes/ejemplo-pipes.component.ts
+++ src/app/ejemplo-pipes/ejemplo-pipes.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejemplo-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ejemplo-pipes.component.html',
   styleUrl: './ejemplo-pipes.component.css',
 })
 export class EjemploPipesComponent {
   fechaActual = new Date();
+
+  nombres = 'antony alexander';
+
+  monto = 4564433.76;
+
+  porcentaje = 0.2334;
+
+  persona = {
+    nombre: 'Juan',
+    apellido: 'Garcia',
+    rol: 'docente',
+  };
 }
